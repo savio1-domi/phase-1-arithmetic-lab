8666ae5ca8cc7fb49f06f10803fb34aad6452d6d
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-13 16:48:51 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+// equation whose result will equal 62
+let num1 = 31;
+let num2 = 2;
+
+let multiply;
+multiply = num1 * num2
+multiply = 62;
+
+// generates a random interger greater than 0
+let random=Math.floor(Math.random()*1000+1)
+
+// an equation that calculates a remainder that is equal to 4
+let num3 =10;
+let num4 = 6;
+let mod = num3 % num4;
+
+// return 20 as the highest number in the set
+let  max=Math.max(3,6,18,20,9);
